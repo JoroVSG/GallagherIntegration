fa2dfa9ee2aee0c36968cff3dbfb26da47279ceb
JoroVSG <joro@vsgbg.com>
2020-06-22 08:42:13 +0300

    Init

---
+++ RodeoDE/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace RodeoDE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ipep = new IPEndPoint(IPAddress.Any, 15000);
+
+            var newsock = new UdpClient(ipep);
+
+            var sender = new IPEndPoint(IPAddress.Any, 0);
+
+            var data = newsock.Receive(ref sender);
+        
+            
+            RodeoParser.Say.getSessionsTask(sender.Address.ToString()).Wait();
+            // var address = RodeoParser.Say.discoveryEndpointTask().Result;
+            // Console.WriteLine(address.ToString());
+            
+        }
+    }
+}
---
+++ RodeoParser/Library.fs
@@ -0,0 +1,65 @@
+﻿namespace RodeoParser
+
+open System.Net
+open System.Net.Sockets
+open System.Text
+open System.Threading.Tasks
+
+module Say =
+    open FSharp.Data
+    let BASE_URL = "http://192.168.43.73:15001";
+    
+    type GallagherType = XmlProvider<Schema="./AnimalSchemaProposal.xsd">
+    
+    let discoveryEndpoint () =
+        
+        let ipep = new IPEndPoint(IPAddress.Any, 15000);
+
+        let newsock = new UdpClient(ipep);
+
+        let sender = new IPEndPoint(IPAddress.Any, 0);
+
+        let data = newsock.Receive(ref sender);
+        
+        sender.Address.ToString()
+        
+        
+        
+       
+            
+    let getAsync url =
+        
+        async {
+            let result = discoveryEndpoint()
+            
+            let httpClient = new System.Net.Http.HttpClient()
+            let! response = httpClient.GetAsync(result + url) |> Async.AwaitTask
+            response.EnsureSuccessStatusCode () |> ignore
+            let! content = response.Content.ReadAsStringAsync() |> Async.AwaitTask
+            return content
+        }
+    let getSession (url) =
+      async {
+          let! animalsResponse = getAsync (url + "/session");
+          let animals = GallagherType.Parse animalsResponse
+          
+          if (animals.Sessions.IsSome) then
+             for session in animals.Sessions.Value.XElement.Descendants() do
+             let s = GallagherType.Session(session)
+             if s.Name.IsSome then
+                printfn "%s" s.Name.Value
+          else
+             printfn "%s" animals.Header.CreatedBy
+      }
+      
+    let getSessionsTask (url) =
+        let task = new Task(fun () -> getSession(url) |> Async.StartImmediate)
+        task.RunSynchronously()
+        task
+    
+    let discoveryEndpointTask () =
+        let task = discoveryEndpoint()
+        //task.RunSynchronously();
+        task
+      
+   
